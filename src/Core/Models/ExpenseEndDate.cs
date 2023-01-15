de7077d6ad50d9264f42aecc585c52dcff6b8f4e
Renan Johny Ortega <renanortega82@gmail.com>
2023-01-14 21:40:38 -0300

    feat: add models.

---
+++ src/Core/Models/ExpenseEndDate.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Models
+{
+    public class ExpenseEndDate
+    {
+        public ExpenseEndDate(DateTime date)
+        {
+            Date = date;
+        }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ src/Core/Models/ExpenseStartDate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class ExpenseStartDate
+    {
+        public DateTime Date { get; set; }
+
+        public ExpenseStartDate(DateTime date)
+        {
+            Date = date;
+        }
+    }
+}
