28b5dd374650df7c8a80d456e5c9418f13f418e8
Renan Johny Ortega <renanortega82@gmail.com>
2023-01-14 21:38:22 -0300

    feat: add controllers.

---
+++ src/Api/Controllers/ExpenseController.cs
@@ -0,0 +1,75 @@
+﻿using Core.Entities;
+using Core.Infrastructure;
+using Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class ExpenseController : ControllerBase
+    {
+        private readonly IExpenseService _expenseService;
+        public ExpenseController(IExpenseService expenseService)
+        {
+            _expenseService = expenseService;
+        }
+
+        // GET v1/<ExpenseController>
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ExpenseDto>>> GetAllAsync()
+        {
+            var expenses = await _expenseService.GetAllAsync();
+            return Ok(expenses);
+        }
+
+        // GET v1/<ExpenseController>/5
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ExpenseDto>> GetByIdAsync([FromRoute] Guid id)
+        {
+            var expense = await _expenseService.GetByIdAsync(id);
+            return Ok(expense);
+        }
+
+        // POST v1/<ExpenseController>
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="createExpenseDto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult<ExpenseDto>> PostAsync([FromBody] CreateExpenseDto createExpenseDto)
+        {
+            var expense = new Expense()
+            {
+                Email = createExpenseDto.Email,
+                Title = createExpenseDto.Title,
+                Value = createExpenseDto.Value,
+                Type = createExpenseDto.Type,
+                Start = createExpenseDto.Start,
+                End = createExpenseDto.End,
+                Created = DateTimeOffset.Now
+            };
+
+            await _expenseService.CreateAsync(expense);
+
+            return CreatedAtAction(nameof(GetByIdAsync), new { id = expense.Id }, expense);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync([FromBody] UpdateExpenseDto updateExpenseDto, [FromRoute] Guid id)
+        {
+            await _expenseService.UpdateAsync(updateExpenseDto, id);
+            return NoContent();
+        }
+    }
+}
---
+++ src/Api/Controllers/ExpenseTotalController.cs
@@ -0,0 +1,61 @@
+﻿using Core.Entities;
+using Core.Infrastructure;
+using Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class ExpenseTotalController : ControllerBase
+    {
+        private readonly IExpenseTotalService _expenseTotalService;
+
+        public ExpenseTotalController(IExpenseTotalService expenseTotalService)
+        {
+            _expenseTotalService = expenseTotalService;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ExpenseTotalDto>> GetByIdAsync(Guid id)
+        {
+            var expenseTotal = await _expenseTotalService.GetAsync(id);
+            return Ok(expenseTotal);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ExpenseTotalDto>>> GetAllAsync()
+        {
+            var expensesTotal = await _expenseTotalService.GetAllAsync();
+            return Ok(expensesTotal);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="createExpenseTotalDto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult<ExpenseTotalDto>> PostAsync([FromBody] CreateExpenseTotalDto createExpenseTotalDto)
+        {
+            var date = new DateTime(createExpenseTotalDto.Date.Year, createExpenseTotalDto.Date.Month, 1);
+
+            var expenseTotal = new ExpenseTotal
+            {
+                Date = date,
+                Email = createExpenseTotalDto.Email,
+                Total = createExpenseTotalDto.Total,
+                Created = DateTimeOffset.Now
+            };
+
+            await _expenseTotalService.FirstExpenseTotalCreation(expenseTotal);
+
+            return CreatedAtAction(nameof(GetByIdAsync), new { id = expenseTotal.Id }, expenseTotal);
+        }
+    }
+}
